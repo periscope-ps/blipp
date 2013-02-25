863e383b7ee652618440e6b204b641d42b7a9d79
mjaffee <mjaffee@indiana.edu>
2013-02-25 17:18:15 -0500

    add some error handling to mac matching... returns False if either mac is not found

--- blipp/geni_utils.py
+++ blipp/geni_utils.py
@@ -1,6 +1,15 @@
 from utils import clean_mac
 
 def mac_match(geni_port, local_port):
-    geni_mac = clean_mac(geni_port['properties']['geni']['mac_address'])
-    local_mac = clean_mac(local_port['properties']['mac']['address'])
+    try:
+        geni_mac = clean_mac(geni_port['properties']['geni']['mac_address'])
+    except KeyError:
+        try:
+            geni_mac = clean_mac(geni_port['properties']['mac']['address'])
+        except KeyError:
+            return False
+    try:
+        local_mac = clean_mac(local_port['properties']['mac']['address'])
+    except KeyError:
+        return False
     return local_mac == geni_mac
