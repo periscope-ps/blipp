ed2ccb9e59af02a2d81de88b2e37bc2de3127283
mjaffee <mjaffee@indiana.edu>
2013-03-22 16:02:29 -0400

    forgot to add mem tests I wrote a while back

---
+++ test/test_mem.py
@@ -0,0 +1,37 @@
+'''
+Created on Dec 11, 2012
+
+@author: jaffee
+'''
+import unittest2
+import mock
+from cStringIO import StringIO
+import consts
+import blipp.mem
+
+class MemProbeTests(unittest2.TestCase):
+    def setUp(self):
+        self.probe = blipp.mem.Probe()
+        self.proc = mock.Mock()
+        self.proc.open.return_value = StringIO(consts.meminfo)
+        self.probe._proc = self.proc
+
+    def test_get_data(self):
+        self.proc.exists.return_value = StringIO(consts.user_beancounters)
+        expected = {"ps:tools:blipp:linux:memory:utilization:free": 525460,
+                    "ps:tools:blipp:linux:memory:utilization:kernel": 60474,
+                    "ps:tools:blipp:linux:memory:utilization:used": 4693144}
+        actual = self.probe.get_data()
+        self.assertEqual(actual, expected)
+
+    def test_get_data2(self):
+        self.proc.exists.return_value = False
+        expected = {"ps:tools:blipp:linux:memory:utilization:free": 525460,
+                    "ps:tools:blipp:linux:memory:utilization:used": 7602504,
+                    "ps:tools:blipp:linux:memory:utilization:buffer": 358700,
+                    "ps:tools:blipp:linux:memory:utilization:cache": 4390548,
+                    "ps:tools:blipp:linux:memory:utilization:kernel": 401292}
+
+        actual = self.probe.get_data()
+        self.assertEqual(actual, expected)
+
