bf47f7d6720da73fcc3c833dfe314fe398cdc47e
josh-bartz <jbartzy@gmail.com>
2013-06-11 09:14:58 -0400

    Updated event types. Adding nodes to UNIS with /dev/ path and UUID as properties.  Still need to check against existing nodes and provide a link to the node in the metadata.

--- blipp/disk.py
+++ blipp/disk.py
@@ -1,7 +1,13 @@
 import resource
 import os
 import math
-from utils import full_event_types
+import sys
+import ethtool
+import socket
+from unis_client import UNISInstance
+import settings
+from utils import full_event_types, blipp_import_method
+import subprocess
 
 class Proc:
 
@@ -23,7 +29,31 @@ class Probe:
         self.config = config
         kwargs = config.get("kwargs", {})
         self._proc = Proc(kwargs.get("proc_dir", "/proc/"))
+        self.unis = None
+
+    def _get_unis(self):
+        if not self.unis:
+            self.unis = UNISInstance(self.config)
+        return self.unis
+
+    def partition_info(self, partition={}):
+        hostName = socket.gethostname()
+        result = {}
+        result['properties'] = {}
+        result['name'] = "%s %s" % (hostName, partition['dev'])
+        lines = subprocess.check_output(["ls", "-l","/dev/disk/by-uuid"])
+        lines = lines.splitlines()
+
+        for line in lines:
+            if line == '': continue
+            line = line.rstrip('\r\n')
+            if line[-4:] == partition['dev']:
+                split = line.split()
+                result['properties']['uuid'] = split[8]
+                result['properties']['path'] = "/dev/"+partition['dev']
         
+        return result
+
     def parse_diskstats(self, dev=None):
         columns = ['m', 'mm', 'dev', 'reads', 'reads_merged', 'sectors_read',
                   'ms_reading', 'writes', 'writes_merged', 'sectors_written', 
@@ -49,45 +79,66 @@ class Probe:
 
         return result #return dict entries for each device
 
-    def calc_reads(self, dataSet, driveType):
-        drive = dataSet[driveType]
+    #def calc_reads(self, dataSet, driveType):
+        #drive = dataSet[driveType]
+    def calc_reads(self, drive):
         numReads = float(drive['reads'])
         readTime = float(drive['ms_reading'])
-        readAvg = round(readTime/numReads)
+        if numReads != 0 and readTime != 0:
+            readAvg = round(readTime/numReads)
+        else:
+            readAvg = 0
         return readAvg
 
-    def calc_writes(self, dataSet, driveType):
-        drive = dataSet[driveType]
+    #def calc_writes(self, dataSet, driveType):
+        #drive = dataSet[driveType]
+    def calc_writes(self, drive):
         numWrites = float(drive['writes'])
         writeTime = float(drive['ms_writing'])
-        writeAvg = round(writeTime/numWrites)
+        if numWrites != 0 and writeTime != 0:
+            writeAvg = round(writeTime/numWrites)
+        else:
+            writeAvg = 0
         return writeAvg
 
     def get_data(self):
-
+        hereCount = 0
+        data = {}
         result = {}
+        thisPartition = {}
         dataSet = self.parse_diskstats()
+        f = open('./tempFile.txt', 'r+')
+        partitions = {}
 
+        test = self._get_unis()
         for key in dataSet:
-            if key[0:2] == 'sd' and len(key) == 3:
-                thisKey = key[0:3]
-                readString = thisKey + ' reads'
-                writeString = thisKey + ' writes'
-                result[readString] = self.calc_reads(dataSet, thisKey)
-                result[writeString] = self.calc_writes(dataSet, thisKey)
+            if key[0:2] == 'sd' and len(key) > 3:
+                partitions[key] = dataSet[key]
+        
+        for key in partitions:
+            #f.write(partitions[key]['dev'])
+            thisPartition = self.partition_info(partitions[key])
+            ref = self._find_or_post_node(thisPartition)
+            hereCount += 1
+            data['write'] = self.calc_writes(partitions[key])
+            data['read'] = self.calc_reads(partitions[key])
+            result[key] = full_event_types(data, EVENT_TYPES)
+            #result[key]['selfRef'] = "test"
+            #f.write("%s" %data)
 
-            elif key[0:2] == 'hd' and len(key) == 3:
-                thisKey = key[0:3]
-                result[thisKey] = self.calc_reads(dataSet, thisKey)
-                writeString = thisKey + ' writes'
-                result[readString] = self.calc_reads(dataSet, thisKey)
-                result[writeString] = self.calc_writes(dataSet, thisKey)
-
+        f.write("%s" %result)
         return result
+
+    def _find_or_post_node(self, partition={}):
+        #f = open('./tempFile.txt', 'r+')
+        #sampleNode = {'name': 'testNode', 'other': 'othertest'}
+        #sampleNode['properties'] = {}
+        #sampleNode['properties']['path'] = "/dev/sda5"
+        #postedNode = self.unis.post("/nodes", sampleNode)
+        #f.write(postedNode["selfRef"])
+        ref = self.unis.post("/nodes", partition)
+        return ref
+
 
 #myProbe = Probe()
 #myProbe.get_data()
-#myProbe.__init__()
-#result = myProbe.get_data() #get the list of dict entries
-#entry = result['sda'] #get the entry for sda
-#print entry['ms_writing'] #print ms_writing
