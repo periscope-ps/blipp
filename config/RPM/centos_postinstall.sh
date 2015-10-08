27c042cb30e3f89240d4bb85ef0f443daad7f41f
jayaajay <jayaajay@indiana.edu>
2015-10-08 12:31:43 -0400

    adding the config file

--- config/RPM/centos_postinstall.sh
+++ config/RPM/centos_postinstall.sh
@@ -19,6 +19,8 @@ chown ${USER}:${USER} ${LOG}
 if grep -q -i "release 6" /etc/redhat-release
 then
   cp ${SHARE}/blippd /etc/init.d/blippd
+  chmod +x /etc/init.d/blippd
+  chown root:root /etc/init.d/blippd
   chkconfig --add blippd
 elif grep -q -i "release 7" /etc/redhat-release
   cp ${SHARE}/blippd.conf ${PETC}/
