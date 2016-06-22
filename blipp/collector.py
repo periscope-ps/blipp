af72dbe6451faa71cc76619fd3363dab42a84287
Jayashree Candadai <jayaajay@indiana.edu>
2016-06-22 14:27:05 -0400

    Adding the license header and license file

--- blipp/collector.py
+++ blipp/collector.py
@@ -1,3 +1,15 @@
+# =============================================================================
+#  periscope-ps (blipp)
+#
+#  Copyright (c) 2013-2016, Trustees of Indiana University,
+#  All rights reserved.
+#
+#  This software may be modified and distributed under the terms of the BSD
+#  license.  See the COPYING file for details.
+#
+#  This software was created at the Indiana University Center for Research in
+#  Extreme Scale Technologies (CREST).
+# =============================================================================
 from ms_client import MSInstance
 from data_logger import DataLogger
 from unis_client import UNISInstance
