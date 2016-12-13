0acc3993e0a62b69de9a30fc3fee15cff674f6dc
Miao Zhang <miaozhan@indiana.edu>
2016-12-12 20:04:48 -0500

    for the simple scheduler, run immediately

--- blipp/schedules/builtins.py
+++ blipp/schedules/builtins.py
@@ -42,9 +42,9 @@ def simple(service, measurement):
         start_time = time.time()
 
     while start_time<end_time:
-        start_time += every
         yield start_time
-
+        start_time += every
+        
 # def recursive_rep(tup_list, start_time=None):
 #     if not start_time:
 #         start_time = time.time()
